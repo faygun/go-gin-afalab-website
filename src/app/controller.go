bfd8c585fe2defc94d00daa9abbf894b4d1940c9
aaygun <aaygun@HNLT09293>
2020-11-06 14:32:23 +0000

    added more routers

--- src/app/controller.go
+++ src/app/controller.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,12 +13,24 @@ func registerRoutes() *gin.Engine {
 	r.LoadHTMLGlob("templates/**/*.html")
 
 	r.Any("/", func(c *gin.Context) {
-		// r.SetHTMLTemplate(template.Must(template.ParseFiles("templates/includes/_layout.html", "templates/views/index.html")))
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
 	r.GET("/about", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "about.html", nil)
+	})
+
+	r.GET("/features", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "features.html", nil)
+	})
+
+	r.GET("/clients", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "clients.html", nil)
+	})
+
+	r.GET("/contact", func(c *gin.Context) {
+		fmt.Println(34)
+		c.HTML(http.StatusOK, "contact.html", nil)
 	})
 
 	r.Static("/public", "./public")
