17b4796f7f0fee4d94d641408665aa9da10e91b1
faygun <faygun89@gmail.com>
2020-11-08 15:32:22 +0000

    controller was developed due the menu link whether is active or not

--- src/app/controller.go
+++ src/app/controller.go
@@ -7,30 +7,46 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type PageSetting struct {
+	PageTitle       string
+	PageDescription string
+	CurrentPage     string
+}
+
 func registerRoutes() *gin.Engine {
 	r := gin.Default()
 
 	r.LoadHTMLGlob("templates/**/*.html")
 
 	r.Any("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
+		c.HTML(http.StatusOK, "index.html", PageSetting{
+			CurrentPage: "home",
+		})
 	})
 
 	r.GET("/about", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "about.html", nil)
+		c.HTML(http.StatusOK, "about.html", PageSetting{
+			CurrentPage: "about",
+		})
 	})
 
 	r.GET("/features", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "features.html", nil)
+		c.HTML(http.StatusOK, "features.html", PageSetting{
+			CurrentPage: "features",
+		})
 	})
 
 	r.GET("/clients", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "clients.html", nil)
+		c.HTML(http.StatusOK, "clients.html", PageSetting{
+			CurrentPage: "clients",
+		})
 	})
 
 	r.GET("/contact", func(c *gin.Context) {
 		fmt.Println(34)
-		c.HTML(http.StatusOK, "contact.html", nil)
+		c.HTML(http.StatusOK, "contact.html", PageSetting{
+			CurrentPage: "contact",
+		})
 	})
 
 	r.Static("/public", "./public")
