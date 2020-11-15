e1d0f86959c853d7d0421638e15e384145eb74d5
faygun <faygun89@gmail.com>
2020-11-15 16:25:20 +0000

    controller layer was updated

--- src/app/controller.go
+++ src/app/controller.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"net/smtp"
+	"os"
+	"path"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -45,13 +50,74 @@ func registerRoutes() *gin.Engine {
 	})
 
 	r.GET("/contact", func(c *gin.Context) {
-		fmt.Println(34)
 		c.HTML(http.StatusOK, "contact.html", PageSetting{
 			CurrentPage: "contact",
 		})
 	})
 
+	r.POST("/contact", func(c *gin.Context) {
+
+		email, _ := c.GetPostForm("email")
+		username, _ := c.GetPostForm("username")
+		message, _ := c.GetPostForm("message")
+
+		date := time.Now().String()
+
+		info := fmt.Sprintf("date: %s \nusername: %s \nemail: %s \nmessage: %s\n\n", date, username, email, message)
+
+		writeText(info)
+		c.JSON(http.StatusOK, "Your message has been successfully sent.")
+	})
+
 	r.Static("/public", "./public")
 
 	return r
+}
+
+func writeText(s string) {
+	base := string(http.Dir("public"))
+
+	path := path.Join(base, "file", "email.txt")
+	// file, err := os.Stat(path)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	f, err := os.OpenFile(path, os.O_APPEND, 0644)
+	// if file != nil {
+	// 	f, err := os.OpenFile(path, os.O_RDWR, 0644)
+	// }
+	// if file == nil {
+	// 	f, err := os.Create(path)
+	// }
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	_, err2 := f.WriteString(s)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+}
+
+func sendMail() {
+	// Set up authentication information.
+	auth := smtp.PlainAuth("", "user@example.com", "password", "mail.example.com")
+
+	// Connect to the server, authenticate, set the sender and recipient,
+	// and send the email all in one step.
+	to := []string{"recipient@example.net"}
+	msg := []byte("To: recipient@example.net\r\n" +
+		"Subject: discount Gophers!\r\n" +
+		"\r\n" +
+		"This is the email body.\r\n")
+	err := smtp.SendMail("mail.example.com:25", auth, "sender@example.org", to, msg)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
